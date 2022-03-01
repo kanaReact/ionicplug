728a5a2d7a91d512bab5af202e98f9059b1f2a00
kanacn@capitalnumbers.com <kanacn@capitalnumbers.com>
2022-03-01 19:01:33 +0530

    initital cooment

---
+++ ios/App/App/IonicNativePluginExample.m
@@ -0,0 +1,8 @@
+//
+//  IonicNativePluginExample.m
+//  App
+//
+//  Created by Admin on 08/02/22.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/App/App/IonicNativePluginExample.swift
@@ -0,0 +1,8 @@
+//
+//  IonicNativePluginExample.swift
+//  App
+//
+//  Created by Admin on 08/02/22.
+//
+
+import Foundation
